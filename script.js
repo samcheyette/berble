a08aef6ef3dd88150f22756656363129064d4c40
samcheyette <sjcheyette@gmail.com>
2022-04-13 14:10:34 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,273 @@
+import { WORDS } from "./words.js";
+import * as wordleAI from './wordleAI.js';
+
+var remainingWords = WORDS.slice();
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+let startTime = new Date().getTime();
+let timeLimit = 90;
+
+console.log(rightGuessString)
+
+
+function timer(minutes, seconds) {
+
+    if ((minutes == 0) & (seconds == 0)) {
+        document.getElementById("timer").innerHTML = "Time's up!";
+        toastr.error("You've run out of time! Game over!")
+    } else {
+        if (seconds < 10) {
+            document.getElementById("timer").innerHTML = minutes + ":0" + seconds;
+        } else {
+            document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+
+        }
+    }
+
+};
+
+function timer_wrapper() {
+    let currentTime = new Date().getTime();
+    var timeElapsed = currentTime-startTime;
+    var secondsElapsed = timeElapsed / 1000;
+    var secondsRemain = timeLimit - secondsElapsed;
+
+    if ((secondsRemain >= 0) & (guessesRemaining > 0)) {
+        var minutes = Math.floor(secondsRemain / 60);
+        var seconds = Math.floor(secondsRemain % 60);
+        timer(minutes, seconds);
+    } else {
+        clearInterval(timer_wrapper);
+
+    }
+
+};
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+    setInterval(timer_wrapper, 1000);
+
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+        
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+function shadeKeyBoard(letter, color) {
+    for (const elem of document.getElementsByClassName("keyboard-button")) {
+        if (elem.textContent === letter) {
+            let oldColor = elem.style.backgroundColor
+            if (oldColor === 'green') {
+                return
+            } 
+
+            if (oldColor === 'yellow' && color !== 'green') {
+                return
+            }
+
+            elem.style.backgroundColor = color
+            break
+        }
+    }
+}
+
+function deleteLetter () {
+    let row = document.getElementsByClassName("letter-row")[NUMBER_OF_GUESSES - guessesRemaining]
+    let box = row.children[nextLetter - 1]
+    box.textContent = ""
+    box.classList.remove("filled-box")
+    currentGuess.pop()
+    nextLetter -= 1
+}
+
+
+
+function checkGuess () {
+    let row = document.getElementsByClassName("letter-row")[NUMBER_OF_GUESSES - guessesRemaining]
+    let guessString = ''
+    let rightGuess = Array.from(rightGuessString)
+
+    for (const val of currentGuess) {
+        guessString += val
+    }
+
+    if (guessString.length != 5) {
+        toastr.error("Not enough letters!")
+        return
+    }
+
+    if (!WORDS.includes(guessString)) {
+        toastr.error("Word not in list!")
+        return
+    }
+
+    remainingWords = wordleAI.getRemainingWords(rightGuessString, remainingWords.slice(), guessString);
+    //console.log(remainingWords);
+    for (let i = 0; i < 5; i++) {
+        let letterColor = ''
+        let box = row.children[i]
+        let letter = currentGuess[i]
+        
+        let letterPosition = rightGuess.indexOf(currentGuess[i])
+        // is letter in the correct guess
+        if (letterPosition === -1) {
+            letterColor = 'grey'
+        } else {
+            // now, letter is definitely in word
+            // if letter index and right guess index are the same
+            // letter is in the right position 
+            if (currentGuess[i] === rightGuess[i]) {
+                // shade green 
+                letterColor = 'green'
+            } else {
+                // shade box yellow
+                letterColor = 'yellow'
+            }
+
+            rightGuess[letterPosition] = "#"
+        }
+
+        let delay = 250 * i
+        setTimeout(()=> {
+            //flip box
+            animateCSS(box, 'flipInX')
+            //shade box
+            box.style.backgroundColor = letterColor
+            shadeKeyBoard(letter, letterColor)
+        }, delay)
+    }
+
+    if (guessString === rightGuessString) {
+        clearInterval(timer_wrapper);
+        toastr.success("You guessed right! Game over!")
+        guessesRemaining = 0
+        return
+    } else {
+        guessesRemaining -= 1;
+        currentGuess = [];
+        nextLetter = 0;
+
+        if (guessesRemaining === 0) {
+            clearInterval(timer_wrapper);
+
+            toastr.error("You've run out of guesses! Game over!")
+            toastr.info(`The right word was: "${rightGuessString}"`)
+        }
+    }
+}
+
+function insertLetter (pressedKey) {
+    if (nextLetter === 5) {
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName("letter-row")[NUMBER_OF_GUESSES - guessesRemaining]
+    let box = row.children[nextLetter]
+    animateCSS(box, "pulse")
+    box.textContent = pressedKey
+    box.classList.add("filled-box")
+    currentGuess.push(pressedKey)
+    nextLetter += 1
+}
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    // const node = document.querySelector(element);
+    const node = element
+    node.style.setProperty('--animate-duration', '0.3s');
+    
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+});
+
+document.addEventListener("keyup", (e) => {
+
+    let currentTime = new Date().getTime();
+    var timeElapsed = currentTime-startTime;
+    var secondsElapsed = timeElapsed / 1000;
+    var secondsRemain = Math.max(0, timeLimit - secondsElapsed);
+
+    let pressedKey = String(e.key)
+
+
+    if ((guessesRemaining === 0) || (secondsRemain <= 0)) {
+        if (pressedKey === "Enter") {
+            document.getElementById("new-game").click();
+        }
+        return
+    }
+
+    if (pressedKey == "Tab") {
+        let AIGuess = wordleAI.makeGuess(WORDS,remainingWords,
+                    Math.min(2500,WORDS.length),75);
+        for (const letter of AIGuess) {
+            insertLetter(letter);
+        }
+        checkGuess()
+
+    }
+
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+    const target = e.target
+
+    if (!target.classList.contains("keyboard-button")) {
+        return
+    }
+    let key = target.textContent
+
+    console.log(key);
+    if (key === "Del") {
+        key = "Backspace";
+    } 
+
+    if (key === "AI") {
+        key = "Tab";
+    }
+
+
+    document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+})
+
+initBoard();
---
+++ wordleAI.js
@@ -0,0 +1,108 @@
+
+export function letInWord(letter, word) {
+    for (const l of word) {
+        if (l == letter) {
+            return true;
+        }
+    }
+    return false;
+};
+
+export function getRemainingWords(true_word, words, guess) {
+    let overlap = [];
+    for (let i=0; i < guess.length; i++) {
+        if (guess[i] == true_word[i]) {
+            overlap[i] = "g";
+        } else if (letInWord(guess[i], true_word)) {
+            overlap[i] = "o";
+        } else {
+            overlap[i] = "r";
+        }
+    }
+    let remaining_words = [];
+    for (const word of words) {
+        let i = 0;
+        let valid = true;
+        while ((i < word.length) & valid) {
+            if (overlap[i] == "g"){
+                if (word[i] != guess[i]){
+                    valid = false;
+                }
+            } else if (overlap[i] == "o") {
+                if (!letInWord(guess[i], word)) {
+                    valid = false;
+                }
+                if (word[i] == guess[i]){
+                    valid = false;
+                }
+            } else if (overlap[i] == "r") {
+                if (letInWord(guess[i], word)) {
+                    valid = false;
+                }
+            }
+            i++;
+
+        }
+
+        if (valid){
+            remaining_words[remaining_words.length] = word;
+        }
+
+    }
+    return remaining_words;
+};
+
+function sampleFrom(arr, n) {
+    let sampleSet = new Set();
+    for (let i=0; i < n; i++) {
+        let index = Math.floor(Math.random()*arr.length);
+        sampleSet.add(arr[index]);
+    }
+    return Array.from(sampleSet);
+};
+
+
+
+
+
+function getInfoGain(guess, words){
+    var pRemain = 0.;
+    for (const word of words) {
+        let nRemain = (getRemainingWords(word, words, guess)).length;
+        pRemain += nRemain/(words.length);    
+    }
+    
+    pRemain = pRemain / words.length;
+
+    return(pRemain);
+};
+
+export function makeGuess(allWords, remainingWords, nSubsampleGuesses, nSubsampleWords) {
+    if (remainingWords.length <= 3) {
+        return (remainingWords[0]);
+    } else {
+
+        let sampleGuesses1 = sampleFrom(remainingWords, 25);
+        let sampleGuesses2 = sampleFrom(allWords, nSubsampleGuesses);
+        let sampleGuesses = sampleGuesses1.concat(sampleGuesses2);
+
+        let sampleWords = sampleFrom(remainingWords, nSubsampleWords);
+        let sampleWords2 = sampleFrom(remainingWords, nSubsampleWords);
+
+        var bestSoFar = "";
+        var bestRemain = 1.;
+
+        for (const sampleGuess of sampleGuesses) {
+            let pRemain = getInfoGain(sampleGuess, sampleWords);
+            if (pRemain <= bestRemain) {
+                bestSoFar = sampleGuess;
+                bestRemain = pRemain;
+
+            }
+        }
+        return bestSoFar;
+
+    }
+
+};
+
